bc7fe82ef18f56d2a38eb1e10f86cfc25197b7f4
Neku <akpotohwoo@gmail.com>
2023-04-23 11:29:33 +0100

    Moo

---
+++ saycow.py
@@ -0,0 +1,37 @@
+import cowsay
+
+message = input("What do you want to say: ")
+character = input("What character do you want to use: ")
+
+
+chars = {
+    'beavis': cowsay.beavis,
+    'cheese': cowsay.cheese,
+    'cow': cowsay.cow,
+    'daemon': cowsay.daemon ,
+    'dragon': cowsay.dragon,
+    'fox': cowsay.fox,
+    'ghostbusters': cowsay.ghostbusters,
+    'kitty': cowsay.kitty,
+    'meow': cowsay.meow,
+    'miki': cowsay.miki,
+    'milk': cowsay.milk,
+    'pig': cowsay.pig,
+    'stegosaurus': cowsay.stegosaurus, 
+    'stimpy': cowsay.stimpy,
+    'trex': cowsay.trex,
+    'turkey': cowsay.turkey,
+    'turtle': cowsay.turtle,
+    'tux': cowsay.tux
+    }
+
+
+if character in chars:
+    chars[character](message)
+else:
+    print(f"Sorry, '{character}' is not a valid character. Valid Characters are:")
+    for _ in chars:
+        print (_)
+
+
+#'fox', 'ghostbusters', 'kitty', 'meow', 'miki', 'milk', 'pig', 'stegosaurus', 'stimpy', 'trex', 'turkey', 'turtle', 'tux'}
