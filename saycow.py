1a9e13f83459b4e3f454f9b1704a1811e6d7f5ee
Chukwuneku <121660030+nekumartins@users.noreply.github.com>
2023-05-07 14:43:11 +0100

    Update saycow.py

--- saycow.py
+++ saycow.py
@@ -1,8 +1,5 @@
 import cowsay
 
-message = input("What do you want to say: ")
-character = input("What character do you want to use: ")
-
 
 chars = {
     'beavis': cowsay.beavis,
@@ -24,6 +21,17 @@ chars = {
     'turtle': cowsay.turtle,
     'tux': cowsay.tux
     }
+print("Hello, User")
+ans = input("To see available lists of charactes input 'y', to skip hit any key")
+if ans.lower() == 'y':
+    for _ in chars:
+        print (_)
+        
+message = input("What do you want to say: ")
+character = input("What character do you want to use: ")
+
+
+
 
 
 if character in chars:
